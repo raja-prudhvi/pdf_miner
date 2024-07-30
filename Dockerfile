98acef4153d2aa17f4dc889a74b6379392d0585a
Tiago Lira <tiago@passthrough.com>
2024-07-30 15:10:53 -0300

    Check if db is up

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 
-RUN apt-get update && apt-get -y install libpq-dev gcc
+RUN apt-get update && apt-get -y install libpq-dev gcc postgresql-client
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+function postgres_ready() {
+  pg_isready -h "${DB_HOST}" > /dev/null 2>&1
+}
+
+until postgres_ready; do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "PostgreSQL is up - executing command"
+
 python manage.py migrate
 exec "$@"
